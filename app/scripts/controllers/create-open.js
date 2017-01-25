33b207e7da7a8f012ab4fe632a11f352234d2ce3
warel <warel@seattleu.edu>
2017-01-25 18:52:01 -0500

    more xml/json testing

--- app/scripts/controllers/create-open.js
+++ app/scripts/controllers/create-open.js
@@ -8,10 +8,10 @@
  * Controller of the workspaceApp
  */
 angular.module('workspaceApp')
-  .controller('CreateCtrl', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CreateCtrl', function ($scope, generateLicense) {
+    $scope.getlicense = function () {
+      var answers = "<answers>   <locale>en</locale>   <license-standard>     <commercial>n</commercial>     <derivatives>y</derivatives>     <jurisdiction></jurisdiction>   </license-standard> </answers>";
+      $scope.licensexml = generateLicense.save ({type: "standard"}, {answers:answers});
+    }
+  
   });
