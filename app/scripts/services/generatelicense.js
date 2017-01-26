6aa637feb439505d668e788af0979324ec2cd26e
warel <warel@seattleu.edu>
2017-01-25 22:18:44 -0500

    reverting back on push call

--- app/scripts/services/generatelicense.js
+++ app/scripts/services/generatelicense.js
@@ -1,14 +1,15 @@
 'use strict';
 
+
 angular.module('workspaceApp')
-  .factory('standard', function ($resource) {
+  .factory('generateLicense', function ($resource) {
 
     // CC Standard API here
-    return $resource('http://creativecommons.org/choose/xhr_api?:query', {}, {
-      find: {
-        method: 'GET',
+    return $resource('http://api.creativecommons.org/rest/1.5/license/:type/issue', {}, {
+      save: {
+        method: 'POST',
         params: {
-          query: "field_commercial=y&field_derivatives=sa&field_jurisdiction=&field_format=&field_worktitle=&field_attribute_to_name=&version=&field_attribute_to_url=&field_sourceurl=&field_morepermissionsurl=&lang="
+          type: "standard"
         },
         isArray: false
       }
--- app/scripts/services/standard.js
+++ app/scripts/services/standard.js
@@ -4,11 +4,11 @@ angular.module('workspaceApp')
   .factory('standard', function ($resource) {
 
     // CC Standard API here
-    return $resource('http://creativecommons.org/choose/xhr_api?:query', {}, {
+    return $resource('http://api.creativecommons.org/rest/dev/license/:query', {}, {
       find: {
         method: 'GET',
         params: {
-          query: "field_commercial=y&field_derivatives=sa&field_jurisdiction=&field_format=&field_worktitle=&field_attribute_to_name=&version=&field_attribute_to_url=&field_sourceurl=&field_morepermissionsurl=&lang="
+          query: "standard"
         },
         isArray: false
       }
